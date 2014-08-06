839b3819ad27bd78bad7c0596bdf4f8b487bb0a3
Jacob Jewell <jacobjewell@eflexystems.com>
2014-08-06 10:29:44 -0400

    add encode function

--- index.js
+++ index.js
@@ -3,12 +3,21 @@
 var bcd    = require('bcd');
 var moment = require('moment');
 
-function _getMillisecond(bytes) {
+function _decodeMillisecond(bytes) {
     var number           = bcd.decode(bytes);
     var numberString     = number.toString();
     var lastDigitRemoved = numberString.slice(0, numberString.length - 1);
 
     return parseInt(lastDigitRemoved);
+}
+
+function _encodeMilliseconds(wrappedDate) {
+  var millisecond = wrappedDate.format('SSS');
+
+  var msd = millisecond.slice(0, 2);
+  var lsd = millisecond.slice(2, 3) + wrappedDate.weekday();
+
+  return Buffer.concat([bcd.encode(msd), bcd.encode(parseInt(lsd))]);
 }
 
 function decode(bytes) {
@@ -19,10 +28,36 @@ function decode(bytes) {
       hour:        bcd.decode(bytes.slice(3, 4)),
       minute:      bcd.decode(bytes.slice(4, 5)),
       second:      bcd.decode(bytes.slice(5, 6)),
-      millisecond: _getMillisecond(bytes.slice(6, 8))
+      millisecond: _decodeMillisecond(bytes.slice(6, 8))
   };
 
   return moment(date).toDate();
 }
 
-module.exports = decode;
+function encode(date) {
+  var wrappedDate = moment(date);
+
+  var splitDate = {
+    year: wrappedDate.year() - 2000,
+    month: wrappedDate.month() + 1,
+    day:  wrappedDate.date(),
+    hour: wrappedDate.hour(),
+    minute: wrappedDate.minute(),
+    second: wrappedDate.second(),
+  };
+
+  var buffer = new Buffer(0);
+  for (var prop in splitDate) {
+    var bytes = bcd.encode(splitDate[prop]);
+    buffer = Buffer.concat([buffer, bytes]);
+  }
+
+  var millisecond = _encodeMilliseconds(wrappedDate);
+
+  buffer = Buffer.concat([buffer, millisecond]);
+
+  return buffer;
+}
+
+module.exports.decode = decode;
+module.exports.encode = encode;
--- test/test.js
+++ test/test.js
@@ -6,23 +6,33 @@ var subject = require('../index');
 
 describe("bcd-date", function() {
 
-  it("should translate a bcd-date buffer into a normal date", function() {
+  describe("decode", function() {
 
-    var expected = moment('2010-12-23 11:30:30.123').toDate();
-    var bytes    = new Buffer('1012231130301235', 'hex');
-    var result   = subject(bytes);
+    it("should decode a bcd-date buffer into a normal date", function() {
+      var expected = moment('2010-12-23 11:30:30.123').toDate();
+      var bytes    = new Buffer('1012231130301234', 'hex');
+      var result   = subject.decode(bytes);
 
-    assert.deepEqual(result, expected);
+      assert.deepEqual(result, expected);
+    });
+
+    it("should handle midnight properly", function() {
+      var expected = moment('2010-01-01 00:00:00.123').toDate();
+      var bytes    = new Buffer('1001010000001234', 'hex');
+      var result   = subject.decode(bytes);
+
+      assert.deepEqual(result, expected);
+    });
   });
 
-  it("should handle midnight properly", function() {
+  describe("encode", function() {
+    it("should encode a date into a bcd-date buffer", function() {
+      var encodedDate = moment('2010-12-23 11:30:30.123').toDate();
+      var expected    = new Buffer('1012231130301234', 'hex');
+      var result      = subject.encode(encodedDate);
 
-    var expected = moment('2010-01-01 00:00:00.123').toDate();
-    var bytes    = new Buffer('1001010000001235', 'hex');
-    var result   = subject(bytes);
-
-    assert.deepEqual(result, expected);
+      assert.deepEqual(result, expected);
+    });
   });
-
 });
 
